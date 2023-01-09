559f01f70b87a8067192b774a953cdd2b1c92d3d
hi <hi@DESKTOP-0HTS3JF>
2023-01-09 16:30:00 +0530

    First Project

---
+++ Jenkins/src/main/java/Demo/Test1.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+	@Test
+	public void m1()
+	{
+		Reporter.log("hii",true);
+	}
+
+}
